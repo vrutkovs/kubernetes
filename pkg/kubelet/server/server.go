d9e5a4c1469792ec3cbfe7710ee04fd070524a60
Maciej Szulik <maszulik@redhat.com>
2015-12-21 13:18:06 +0100

    UPSTREAM: 75190: set backup value to ensure connections against kubelets eventually close :100644 100644 c561ddb... 17ef5df... M	pkg/kubelet/server.go

--- pkg/kubelet/server/server.go
+++ pkg/kubelet/server/server.go
@@ -143,6 +143,8 @@ func ListenAndServeKubeletServer(
 	s := &http.Server{
 		Addr:           net.JoinHostPort(address.String(), strconv.FormatUint(uint64(port), 10)),
 		Handler:        &handler,
+		ReadTimeout:    60 * time.Minute,
+		WriteTimeout:   60 * time.Minute,
 		MaxHeaderBytes: 1 << 20,
 	}
 	if tlsOptions != nil {
@@ -164,6 +166,8 @@ func ListenAndServeKubeletReadOnlyServer(host HostInterface, resourceAnalyzer st
 	server := &http.Server{
 		Addr:           net.JoinHostPort(address.String(), strconv.FormatUint(uint64(port), 10)),
 		Handler:        &s,
+		ReadTimeout:    60 * time.Minute,
+		WriteTimeout:   60 * time.Minute,
 		MaxHeaderBytes: 1 << 20,
 	}
 	klog.Fatal(server.ListenAndServe())
