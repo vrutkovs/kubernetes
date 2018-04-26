b1d69092769770bbc9325a50007df90e60ab6667
Seth Jennings <sjenning@redhat.com>
2018-04-26 16:35:47 -0500

    UPSTREAM: <carry>: XFS quota for emptyDir volumes
    Origin-commit: 7331c6412a9ef1b23155d7fd928f4ddc6961a05b

--- cmd/kubelet/app/server.go
+++ cmd/kubelet/app/server.go
@@ -380,7 +380,7 @@ func UnsecuredDependencies(s *options.KubeletServer) (*kubelet.Dependencies, err
 		}
 	}
 
-	return &kubelet.Dependencies{
+	deps := &kubelet.Dependencies{
 		Auth:                nil, // default does not enforce auth[nz]
 		CAdvisorInterface:   nil, // cadvisor.New launches background processes (bg http.ListenAndServe, and some bg cleaners), not set here
 		Cloud:               nil, // cloud provider might start background processes
@@ -396,7 +396,14 @@ func UnsecuredDependencies(s *options.KubeletServer) (*kubelet.Dependencies, err
 		Writer:              writer,
 		VolumePlugins:       ProbeVolumePlugins(),
 		DynamicPluginProber: GetDynamicPluginProber(s.VolumePluginDir),
-		TLSOptions:          tlsOptions}, nil
+		TLSOptions:          tlsOptions,
+	}
+
+	if err := PatchVolumePluginsForLocalQuota(s.RootDirectory, &deps.VolumePlugins); err != nil {
+		return nil, fmt.Errorf("Local quota setup failed: %v", err)
+	}
+
+	return deps, nil
 }
 
 // Run runs the specified KubeletServer with the given Dependencies. This should never exit.
