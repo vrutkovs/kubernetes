23a44dc4d773eb3ca1f927f8afea726771793c68
David Eads <deads@redhat.com>
2018-06-13 16:28:32 -0400

    UPSTREAM: <carry>: Allow overriding default generators for run

--- pkg/kubectl/cmd/run.go
+++ pkg/kubectl/cmd/run.go
@@ -124,6 +124,9 @@ type RunOptions struct {
 	TTY            bool
 
 	genericclioptions.IOStreams
+
+	DefaultRestartAlwaysGenerator string
+	DefaultGenerator              string
 }
 
 func NewRunOptions(streams genericclioptions.IOStreams) *RunOptions {
@@ -301,6 +304,16 @@ func (o *RunOptions) Run(f cmdutil.Factory, cmd *cobra.Command, args []string) e
 	if len(o.Schedule) != 0 && len(generatorName) == 0 {
 		generatorName = cmdutil.CronJobV1Beta1GeneratorName
 	}
+
+	if len(generatorName) == 0 {
+		switch {
+		case restartPolicy == api.RestartPolicyAlways:
+			generatorName = o.DefaultRestartAlwaysGenerator
+		default:
+			generatorName = o.DefaultGenerator
+		}
+	}
+
 	if len(generatorName) == 0 {
 		switch restartPolicy {
 		case corev1.RestartPolicyAlways:
