e8f6f9a16679d19d2b9be8476b1f930378c29a70
Ted Yu <yuzhihong@gmail.com>
2019-01-31 09:19:38 -0800

    UPSTREAM: 73599: Add ps1 to windows executable extensions

--- pkg/kubectl/cmd/plugin/plugin.go
+++ pkg/kubectl/cmd/plugin/plugin.go
@@ -228,7 +228,10 @@ func isExecutable(fullPath string) (bool, error) {
 	}
 
 	if runtime.GOOS == "windows" {
-		if strings.HasSuffix(info.Name(), ".exe") {
+		fileExt := strings.ToLower(filepath.Ext(fullPath))
+
+		switch fileExt {
+		case ".bat", ".cmd", ".com", ".exe", ".ps1":
 			return true, nil
 		}
 		return false, nil
