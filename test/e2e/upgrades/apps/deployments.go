openshift/origin

UPSTREAM: 00000: add debugging for upgrade tests

vendor/k8s.io/kubernetes/test/e2e/upgrades/apps/deployments.go
@@ -164,7 +164,11 @@ func (t *DeploymentUpgradeTest) Test(f *framework.Framework, done <-chan struct{
 	framework.ExpectNoError(err)
 
 	ginkgo.By(fmt.Sprintf("Waiting for deployment %q to complete after scaling", deploymentName))
-	framework.ExpectNoError(framework.WaitForDeploymentComplete(c, deployment))
+	err = framework.WaitForDeploymentComplete(c, deployment)
+	if err != nil {
+		framework.DumpAllNamespaceInfo(f.ClientSet, deployment.Namespace)
+	}
+	framework.ExpectNoError(err)
 }
 
 // Teardown cleans up any remaining resources.

vendor/k8s.io/kubernetes/test/e2e/upgrades/apps/replicasets.go
@@ -100,7 +100,12 @@ func (r *ReplicaSetUpgradeTest) Test(f *framework.Framework, done <-chan struct{
 	framework.ExpectNoError(err)
 
 	ginkgo.By(fmt.Sprintf("Waiting for replicaset %s to have all of its replicas ready after scaling", rsName))
-	framework.ExpectNoError(framework.WaitForReadyReplicaSet(c, ns, rsName))
+
+	err = framework.WaitForReadyReplicaSet(c, ns, rsName)
+	if err != nil {
+		framework.DumpAllNamespaceInfo(f.ClientSet, ns)
+	}
+	framework.ExpectNoError(err)
 }
 
 // Teardown cleans up any remaining resources.

vrutkovs/kubernetes

Origin-commit: 2967069b36915643acca58da463753ba5b115759

test/e2e/upgrades/apps/deployments.go
@@ -164,7 +164,11 @@ func (t *DeploymentUpgradeTest) Test(f *framework.Framework, done <-chan struct{
 	framework.ExpectNoError(err)
 
 	ginkgo.By(fmt.Sprintf("Waiting for deployment %q to complete after scaling", deploymentName))
-	framework.ExpectNoError(e2edeploy.WaitForDeploymentComplete(c, deploymentWithUpdatedReplicas))
+	err = e2edeploy.WaitForDeploymentComplete(c, deploymentWithUpdatedReplicas)
+	if err != nil {
+		framework.DumpAllNamespaceInfo(f.ClientSet, deployment.Namespace)
+	}
+	framework.ExpectNoError(err)
 }
 
 // Teardown cleans up any remaining resources.

test/e2e/upgrades/apps/replicasets.go
@@ -95,7 +95,12 @@ func (r *ReplicaSetUpgradeTest) Test(f *framework.Framework, done <-chan struct{
 	framework.ExpectNoError(err)
 
 	ginkgo.By(fmt.Sprintf("Waiting for replicaset %s to have all of its replicas ready after scaling", rsName))
-	framework.ExpectNoError(replicaset.WaitForReadyReplicaSet(c, ns, rsName))
+
+	err = replicaset.WaitForReadyReplicaSet(c, ns, rsName)
+	if err != nil {
+		framework.DumpAllNamespaceInfo(f.ClientSet, ns)
+	}
+	framework.ExpectNoError(err)
 }
 
 // Teardown cleans up any remaining resources.